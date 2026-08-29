f7825ef3d1b6173dda273219ffaccc8d3e36db3c
Minh Chien <EMAIL_GITHUB_CUA_BAN>
2026-08-29 21:29:45 +0700

    Deploy Ly Tuong Song Web v1

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'ly-tuong-song-v1.7';
+const FILES = [
+  '/',
+  '/index.html',
+  '/manifest.webmanifest'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
